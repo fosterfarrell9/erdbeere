1a4976b1f251355c9688063f2996bd8afaac360c
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-02-14 18:38:43 +0100

    run CachePopulator only once when booting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,6 +7,14 @@ Sidekiq.configure_client do |config|
 end
 
 Sidekiq.configure_server do |config|
+  # see https://stackoverflow.com/questions/17837923/queue-sidekiq-job-on-rails-app-start
+  config.on(:startup) do
+    already_scheduled = Sidekiq::ScheduledSet.new.any? do |job|
+      job.klass == "CachePopulator"
+    end
+    CachePopulator.perform_async unless already_scheduled
+  end
+
   # accepts :expiration (optional)
   Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
 
