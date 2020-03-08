a186b4801b64f17a9d3ccc423bf722efd097a3a3
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-03-08 13:46:14 +0100

    change locale behaviour

--- config/routes.rb
+++ config/routes.rb
@@ -3,37 +3,34 @@ Rails.application.routes.draw do
   require 'sidekiq-status/web'
   mount Sidekiq::Web => '/sidekiq'
 
-  root to: redirect('/de')
-  scope '/:locale', locale: /#{I18n.available_locales.join('|')}/ do
-    post '/examples/find'
-    get '/examples/find', to: 'main#search'
-    get '/examples/:id/add_example_facts', to: 'examples#add_example_facts',
-                                           as: 'add_example_facts'
-    post '/examples/:id/update_example_facts',
-         to: 'examples#update_example_facts',
-         as: 'update_example_facts'
-    resources :examples
+  post '/examples/find'
+  get '/examples/find', to: 'main#search'
+  get '/examples/:id/add_example_facts', to: 'examples#add_example_facts',
+                                         as: 'add_example_facts'
+  post '/examples/:id/update_example_facts',
+       to: 'examples#update_example_facts',
+       as: 'update_example_facts'
+  resources :examples
 
-    match 'search', as: 'main_search', via: :get, to: 'main#search'
+  match 'search', as: 'main_search', via: :get, to: 'main#search'
 
-    resources :structures
-    get '/properties/:id/add_example_facts', to: 'properties#add_example_facts',
-                                             as: 'add_example_facts_to_property'
-    post '/properties/:id/update_example_facts',
-         to: 'properties#update_example_facts',
-         as: 'update_example_facts_to_property'
-    resources :properties
-    resources :building_blocks
-    resources :implications
-    resources :axioms
-    resources :example_facts
+  resources :structures
+  get '/properties/:id/add_example_facts', to: 'properties#add_example_facts',
+                                           as: 'add_example_facts_to_property'
+  post '/properties/:id/update_example_facts',
+       to: 'properties#update_example_facts',
+       as: 'update_example_facts_to_property'
+  resources :properties
+  resources :building_blocks
+  resources :implications
+  resources :axioms
+  resources :example_facts
 
-    namespace :api do
-      namespace :v1 do
-        get 'examples/:id', to: 'examples#show'
-      end
+  namespace :api do
+    namespace :v1 do
+      get 'examples/:id', to: 'examples#show'
     end
-
-    get '/' => 'structures#index'
   end
+
+  get '/' => 'structures#index'
 end
